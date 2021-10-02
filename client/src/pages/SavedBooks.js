d56f43d6cb52554829dfd5ae3390a7ef499bdf46
Victor Lupascu <sinajeen@gmail.com>
2021-10-02 01:15:11 -0500

    refactor save books

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import {
   Jumbotron,
   Container,
@@ -7,40 +7,15 @@ import {
   Button,
 } from 'react-bootstrap'
 
-import { getMe, deleteBook } from '../utils/API'
 import Auth from '../utils/auth'
 import { removeBookId } from '../utils/localStorage'
+import { useQuery, useMutation } from '@apollo/client'
+import { QUERY_ME } from '../utils/queries'
+import { REMOVE_BOOK } from '../utils/mutations'
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({})
-
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length
-
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null
-
-        if (!token) {
-          return false
-        }
-
-        const response = await getMe(token)
-
-        if (!response.ok) {
-          throw new Error('something went wrong!')
-        }
-
-        const user = await response.json()
-        setUserData(user)
-      } catch (err) {
-        console.error(err)
-      }
-    }
-
-    getUserData()
-  }, [userDataLength])
+  const { loading, data: userData } = useQuery(QUERY_ME)
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK)
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -51,14 +26,10 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token)
+      const { data: updatedUser } = await removeBook({
+        variables: { bookId },
+      })
 
-      if (!response.ok) {
-        throw new Error('something went wrong!')
-      }
-
-      const updatedUser = await response.json()
-      setUserData(updatedUser)
       // upon success, remove book's id from localStorage
       removeBookId(bookId)
     } catch (err) {
@@ -67,7 +38,7 @@ const SavedBooks = () => {
   }
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>
   }
 
@@ -80,14 +51,14 @@ const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${
-                userData.savedBooks.length === 1 ? 'book' : 'books'
+          {userData.me.savedBooks.length
+            ? `Viewing ${userData.me.savedBooks.length} saved ${
+                userData.me.savedBooks.length === 1 ? 'book' : 'books'
               }:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
-          {userData.savedBooks.map((book) => {
+          {userData.me.savedBooks.map((book) => {
             return (
               <Card key={book.bookId} border="dark">
                 {book.image ? (
