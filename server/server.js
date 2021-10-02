6b677d4ddc0f1f8a4d520582f97066fd89ab0db0
Victor Lupascu <sinajeen@gmail.com>
2021-10-02 15:36:31 -0500

    removed routes

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 const db = require('./config/connection')
-const routes = require('./routes')
 const { authMiddleware } = require('./utils/auth')
 
 // import ApolloServer and its typeDefs and resolvers
@@ -33,7 +32,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')))
 }
 
-app.use(routes)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'))
 })
